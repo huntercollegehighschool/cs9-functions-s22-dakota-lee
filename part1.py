b2954d69ae594e686c926dfc7845ed9d58bb2a38
Dakota Lee <103528292+dakota-lee@users.noreply.github.com>
2022-05-17 17:52:25 +0000

    fixed part 1

--- part1.py
+++ part1.py
@@ -7,8 +7,11 @@ The function distinct() below takes three numbers as arguments, and returns True
 However, there are (at least) 7 errors in the code. Fix them so that it runs properly.
 '''
 
-def distinct()
-if a != b and b!= c: #a, b, and c are the three parameters (numbers) that will be passed into the function
-return True
-elif
-return False
+def distinct(a,b,c):
+#a, b, and c are the three parameters (numbers) that will be passed into the function
+  if a != b and b!= c and a!=c: 
+    return True
+  else:
+    return False
+
+#done
